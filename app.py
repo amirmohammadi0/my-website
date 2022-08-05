f6b781cd92af8c238b7d1fc3de9b00b9ec7245a1
amirmohammadi0 <amirmohammadi00184@gail.com>
2022-08-06 00:45:39 +0430

    add auth di and authentication files

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 from fastapi import FastAPI
 from db.database import Base,engine
 from router import user,article
+from auth import authentication
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 app.include_router(user.router)
 app.include_router(article.router)
+app.include_router(authentication.router)
 
 @app.get('/',tags=['index page'])
 async def index():
---
+++ auth/authentication.py
@@ -0,0 +1,26 @@
+from fastapi import APIRouter, Depends, status
+from db import models
+from db.database import get_db
+from fastapi.security.oauth2 import OAuth2PasswordRequestForm
+from sqlalchemy.orm.session import Session
+from fastapi.exceptions import HTTPException
+from auth import oauth2
+
+
+router = APIRouter(tags=['auth'])
+
+@router.post('/token')
+def get_token(request:OAuth2PasswordRequestForm=Depends(),db:Session=Depends(get_db)):
+    user = db.query(models.User).filter(models.User.username == request.username).first()
+    access_token = oauth2.create_access_token(data={'sub':user.username})
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='not found user')
+    elif user.password != request.password:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='invalid password')
+    else:
+        return {
+        'access_token':access_token,
+        'type_token':'bearer',
+        'userID':user.id,
+        'usename':user.username
+        }
